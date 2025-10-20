3b14d45a8b675ed154326fef9ff247b0f4221c44
Ryan Motchkavitz <ryanmotch2@gmail.com>
2025-10-20 18:21:19 -0400

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
+import { useState} from "react";
+import { tvShows} from "./shows/data";
+import ShowDetails from "./shows/ShowDetails"
+import ShowSelection from "./shows/ShowSelection";
+
+
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+
+
+//COMPONENTS
+
 export default function App() {
+  const [shows] = useState (tvShows);
+  const[selectedShow, setSelectedShow] =useState();
+
   return (
     <>
       <header>
         <p>React TV</p>
-      </header>
-      <main></main>
+        <ShowSelection
+        shows={shows}
+        selectedShow={selectedShow}
+        setSelectedShow={setSelectedShow} />
+        </header>
+
+
+      <main>
+        <ShowDetails key= {selectedShow?.name} show={selectedShow} />
+      </main>
     </>
   );
+  
 }
